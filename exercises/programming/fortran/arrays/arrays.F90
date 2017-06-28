099db144487411d249f0f75da5aa23a9b8ca3fbb
Ben <benjamin.foreback@gmail.com>
2017-06-28 19:04:46 +0300

    Fortran Exercise 3

--- exercises/programming/fortran/arrays/arrays.F90
+++ exercises/programming/fortran/arrays/arrays.F90
@@ -1,21 +1,30 @@
 program arrays
   implicit none
   integer :: nx, ny
-  integer :: i, alloc_stat
+  integer :: i, j, alloc_stat
   ! TODO: define allocatable array A
+
+  real, allocatable :: A(:,:)
 
   write (*,*) 'Give number of rows and columns for matrix A:'
   read (*,*) nx, ny
 
-  ! TODO allocate A now that we know nx and ny
+  if (nx < 0) then
+    write (*,*) 'You cannot supply a negative number.'
+    stop
+  end if
 
+  ! TODO allocate A now that we know nx and ny
+  allocate (A(nx,ny), stat=alloc_stat)
+  if (alloc_stat /= 0) write(*,*) 'ERROR allocating the array A'
 
   ! TODO Use array syntax to initialize the array
 
-
-
-
-
+  do j=1,ny
+    do i=1, nx
+      A(i,j) = i+j
+    end do
+  end do
 
 
   !--------------------------------------------------
