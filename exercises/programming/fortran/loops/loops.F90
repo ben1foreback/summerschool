cf1d31d150375837aa18e2d87d010965f7899aed
Ben <benjamin.foreback@helsinki.fi>
2017-06-28 19:23:59 +0300

    Loops (fortran exercise 2)

--- exercises/programming/fortran/loops/loops.F90
+++ exercises/programming/fortran/loops/loops.F90
@@ -1,11 +1,31 @@
 program loops
   implicit none
   ! TODO define parameters nx and ny
+  integer, parameter :: nx = 10, ny = 10
   ! TODO: define real-valued array A
+  real, dimension(0:nx+1,0:ny+1) :: A
   integer :: i, j
 
-  ! TODO initialize array A here
+  ! Initialize array A, first with values of 65
+  A = 65
 
+  ! Now initialize boundaries, outside the normal bounds.
+  
+  do i=0, nx+1
+    A(i,0) = 20
+  end do
+
+  do i=0, nx+1
+    A(i,ny+1) = 70
+  end do
+
+  do j=0, ny+1
+    A(0,j) = 85
+  end do
+
+  do j=0, ny+1
+    A(nx+1, j) = 5
+  end do
 
 
   !--------------------------------------------------
